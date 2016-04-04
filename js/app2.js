8622c6882ad65445635f7f673b7f7ba62bca8fe2
Haesol <haesol87@gmail.com>
2016-04-04 14:11:33 -0400

    Created app2.js file for testing purposes. Tictactoe has winning conditions, gameboard is disabled when game is done, can reset the game.

--- js/app2.js
+++ js/app2.js
@@ -3,46 +3,46 @@ $(document).ready(function() {
     var $gameCells = $('.cell');
     var moves = ["", "", "", "", "", "", "", "", ""];
     var count = 0;
+
     var turn = chooseTurn();
+    //var moves = Array(9).join(".").split(".");
+    //console.log($gameCells);
+
+
 
 
 
     $($gameCells).on('click', function() {
-        placeMove(this);
+
+        console.log(this.id);
+
+        $(this).html(turn).attr("style", "color: red; text-align: center; line-height:100px;")
+
+        moves[this.id] = turn;
+        count++;
+        turn = (turn == 'X') ? 'O' : 'X';
+        placeMove();
+        checkWin(moves);
+        console.log(moves, count);
     });
 
     $('#newGame').on('click', function() {
         resetBoard();
-    });
 
 
-    function placeMove(this2) {
+
+
+    function placeMove() {
         console.log('Move was placed');
-        console.log(this2.id);
+        // var mark = document.createElement('div');
+        // console.log(square);
 
-        // var $square = $('#gameboard #0');
-
-        $(this2).html(turn).attr("style", "color: red; text-align: center; line-height:100px;")
-
-        moves[this2.id] = turn;
-        count++;
-        turn = (turn == 'X') ? 'O' : 'X';
-
-
-        // if (count % 2 == 0){
-        //   turn = 'X';
-        // } else {
-        //   turn = 'O';
-        // }
-        checkWin(moves, count);
-        // (count % 2 == 0) ? moves[this.id] = "X": moves[this.id] = "O"
-        //moves[this.id] = this.id;
-        console.log(moves, count);
+        // var newDiv = $("<div id = 'x'>'X'</div>");
+        // square.append(newDiv);
     }
 
     //Check whether player "X" or "O" wins
-    function checkWin(move, count) {
-
+    function checkWin(move) {
         if ((move[0] == 'X' && move[1] == 'X' && move[2] == 'X') ||
             (move[3] == 'X' && move[4] == 'X' && move[5] == 'X') ||
             (move[6] == 'X' && move[7] == 'X' && move[8] == 'X') ||
@@ -53,7 +53,8 @@ $(document).ready(function() {
             (move[2] == 'X' && move[4] == 'X' && move[6] == 'X')) {
             window.alert('X wins!');
             disableBoard();
-        } else if ((move[0] == 'O' && move[1] == 'O' && move[2] == 'O') ||
+        }
+        if ((move[0] == 'O' && move[1] == 'O' && move[2] == 'O') ||
             (move[3] == 'O' && move[4] == 'O' && move[5] == 'O') ||
             (move[6] == 'O' && move[7] == 'O' && move[8] == 'O') ||
             (move[0] == 'O' && move[3] == 'O' && move[6] == 'O') ||
@@ -63,20 +64,18 @@ $(document).ready(function() {
             (move[2] == 'O' && move[4] == 'O' && move[6] == 'O')) {
             window.alert("O wins!");
             disableBoard();
-        } else if (count == 9) {
-            window.alert("It's a tie!");
-            disableBoard();
+
         }
     }
 
     function disableBoard() {
-        console.log('Is disableBoard function called?');
+        console.log('Is disableBoard function called?')
         $($gameCells).off("click");
     }
 
     function resetBoard() {
         // console.log('reset board is active');
-        // $($gameCells).on('click');
+        // // $($gameCells).on('click');
         // moves = ["", "", "", "", "", "", "", "", ""];
         // count = 0;
         // $('.cell').removeAttr("style");
@@ -102,13 +101,16 @@ $(document).ready(function() {
 
     }
 
+
+
+
+
+
+
+
+
+    // (count % 2 == 0) ? moves[this.id] = "X": moves[this.id] = "O"
+    // moves[this.id] = (count % 2 ==0) ? 'X':'O'
+
+
 });
-
-
-
-
-
-
-
-// (count % 2 == 0) ? moves[this.id] = "X": moves[this.id] = "O"
-// moves[this.id] = (count % 2 ==0) ? 'X':'O'
